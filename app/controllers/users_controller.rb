f5b6a6c641babbf13a8dce4ff3911961879beea3
Dueno <anoinchick@gmail.com>
2017-07-05 11:24:12 +0900

    pry-railsいれるなどした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :reject_non_admin_user, only: [:manage]
 
   def create
-    @user = User.create params[:user]
+    @user = User.create params.require(:user).permit(:user, :name, :pass, :icon_file_name)
     render json: {errors: @user.errors.full_messages}, status: :bad_request and return if @user.errors.any?
 
     @user = User.find_by user: @user.user
